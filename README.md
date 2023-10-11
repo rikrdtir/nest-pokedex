e7433ca681f55637e79f9855f2c575204ad357d3
rikrdtir <ricardo6066@gmail.com>
2023-10-10 19:07:28 -0500

    variables de entorno unsando joi

--- README.md
+++ README.md
@@ -18,6 +18,19 @@ npm i -g @nestjs/cli
 ```
 docker-compose up -d
 ```
+5. Clonar el archivo __.env.template__, renombrar a __.env__
+
+6. Llegar las variables de entorno definidas en el ```.env```
+
+7. Ejecutar la aplicacion en Dev:
+```
+yarn star:dev
+
+```
+8. Reconstruir la base de datos usando la semilla en la url:
+```
+http://localhost:3000/api/v2/seed
+```
 
 ## Stack usado
 * MongoDB
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,23 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { joiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: joiValidationSchema,
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
+    // MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
     CommonModule,
     SeedModule,
